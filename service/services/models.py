57ea5bec84ac07a98240a3c38cad378d5223c37b
koqfl <koqfloqf@gmail.com>
2024-04-03 15:51:59 +0300

    day two

--- service/services/models.py
+++ service/services/models.py
@@ -4,10 +4,10 @@ from django.db import models
 from clients.models import Client
 
 
+# Create your models here.
 class Service(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     full_price = models.PositiveIntegerField()
-
 
 
 
@@ -15,13 +15,17 @@ class Plan(models.Model):
     PLAN_TYPES = (
         ('full','Full'),
         ('student','Student'),
-        ('discount','Discount'),
+        ('discount','Discount')
     )
 
-    plan_type = models.CharField(choices=PLAN_TYPES,max_length=10)
-    discount_percent = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
-# Create your models here.
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=100)
+    discount_percent = models.PositiveIntegerField(validators=[
+        MaxValueValidator(100),
+    ])
+
+
+
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscription',on_delete=models.PROTECT)
-    service = models.ForeignKey(Service, related_name='subscription',on_delete=models.PROTECT)
-    plan = models.ForeignKey(Plan,related_name='subscription',on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
